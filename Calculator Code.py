648e2affd266240bf0e56114640c78d22f3397c7
SS COMPUTER <jahidjesan508@gmail.com>
2023-08-24 19:56:58 +0600

    update this project code

--- Calculator Code.py
+++ Calculator Code.py
@@ -1,40 +1,24 @@
-# simple project for Calculator
-# call the def function ?
-def add(x, y):
-    return x + y
-
-
-def subtract(x, y):
-    return x - y
-
-
-def multiply(x, y):
-    return x * y
-
-
-def divide(x, y):
-    return x / y
-
-
-print("Please Select Operation:\n"
-      "1. add\n"
-      "2. subtract\n"
-      "3. multiply\n"
-      "4. divide")
-
-Select = int(input("Select Your Math Operation 1, 2, 3, 4 : "))
-# input this two int type data !
-s1 = int(input("Enter This First Numbers:"))
-s2 = int(input("Enter This Second Numbers:"))
-
-# used condition --if-elif-else
-if Select == 1:
-    print(s1, "+", s2, "=", add(s1, s2))
-elif Select == 2:
-    print(s1, "-", s2, "=", subtract(s1, s2))
-elif Select == 3:
-    print(s1, "*", s2, "=", multiply(s1, s2))
-elif Select == 4:
-    print(s1, "/", s2, "=", divide(s1, s2))
+# Calculator project ?
+# create input two value and one operator
+print("Using Two Value And One Operator\n")
+F = float(input("Enter The First Number:"))
+OP = str(input("Enter The Must Use Seven Operator-[+] [-] [*] [/] [%] [**] [//] :"))
+S = float(input("Enter The Second Number:"))
+# Apply to condition !
+# using if-elif-else Three condition
+if OP == "+":
+    print("Addition Value : ", F + S)
+elif OP == "-":
+    print("Subtraction Value :", F - S)
+elif OP == "*":
+    print("Multiplication Value :", F * S)
+elif OP == "/":
+    print("Division Value :", F / S)
+elif OP == "%":
+    print("Modulus Value :", F % S)
+elif OP == "**":
+    print("Exponentiation Value :", F ** S)
+elif OP == "//":
+    print("Floor division Value :", F // S)
 else:
-    print("Invalid Input")
+    print("Wrong Operators")
